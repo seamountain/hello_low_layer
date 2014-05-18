e5908bb6d17595bed39a1ba3ac638d1b21aa93dd
sea_mountain <seaforestmountain@gmail.com>
2014-05-18 23:44:34 +0900

    Fix some logics but there are some bugs

--- hello_malloc.cpp
+++ hello_malloc.cpp
@@ -1,60 +1,42 @@
 #include <iostream>
+#include "cassert"
 
-typedef struct HeapBlock {
+struct HeapBlock {
   struct HeapBlock *prev;
   struct HeapBlock *next;
   int size;
-} HeapBlock;
+};
 
 const int array_size = 10000;
 int arena[array_size];
-struct HeapBlock *alloc_block;
-struct HeapBlock *free_block;
+struct HeapBlock *alloc_block = new HeapBlock();
+struct HeapBlock *free_block = new HeapBlock();
 
 void initialize_block() {
-  alloc_block = (HeapBlock *)&arena;
-  free_block = (HeapBlock *)&arena + sizeof(alloc_block);
-
   alloc_block->next = alloc_block;
-  alloc_block->size = sizeof(arena);
-
-  printf("sizeof(alloc_block): %lu\n", sizeof(alloc_block));
-
-  free_block->prev = (free_block - 1);
-  free_block->next = free_block + 1;
+  alloc_block->size = 0;
+  free_block->prev = (HeapBlock *)arena;
+  free_block->next = (HeapBlock *)&arena[0];
   free_block->size = sizeof(arena);
-
-  alloc_block->size += sizeof(alloc_block);
-  free_block->size -= sizeof(free_block);
-
-  printf("free_block->size: %d\n", free_block->size);
-  printf("alloc_block->size: %d\n", alloc_block->size);
 }
 
 void *orig_malloc(size_t size) {
   struct HeapBlock *new_alloc_block = free_block->next;
-  struct HeapBlock *block_head = free_block->next;
-  block_head->size = size;
-  block_head->prev = alloc_block->next;
-  HeapBlock *next_block = alloc_block->next;
-  if (next_block && !next_block->next) {
-    next_block->next = (HeapBlock *)&arena;
-  }
-  alloc_block->next = block_head;
-
-  alloc_block->next = block_head;
+  new_alloc_block->prev = alloc_block->next;
+  alloc_block->next->prev = new_alloc_block;
+  new_alloc_block->next = alloc_block->prev;
+  new_alloc_block->size = size;
+  alloc_block->next = new_alloc_block;
   alloc_block->size += sizeof(new_alloc_block) + size;
-  free_block->next = alloc_block->next + sizeof(block_head)+ block_head->size + 1;
+
+  free_block->next = (struct HeapBlock*)((size_t)(new_alloc_block + 1) + size);
+  free_block->prev = alloc_block->next;
   free_block->size -= sizeof(new_alloc_block) + size;
 
-  return new_alloc_block + size;
+  return new_alloc_block + 1;
 }
 
 void orig_free(void *ptr) {
-  printf("&ptr: %p\n", &ptr);
-  printf("ptr: %p\n", ptr);
-  printf("sizeof(alloc_block): %lu\n", sizeof(alloc_block));
-
   struct HeapBlock *block_head = (HeapBlock *)(ptr) - sizeof(alloc_block);
 
   if (block_head && alloc_block->next) {
@@ -64,10 +46,6 @@ void orig_free(void *ptr) {
   if (block_head && free_block->next) {
     free_block->next = (HeapBlock *)&arena + sizeof(alloc_block);
   }
-
-  printf("size: %d\n", (int)((HeapBlock *)&ptr)->size);
-  printf("sizeof(ptr): %lu\n", sizeof(ptr));
-  printf("((HeapBlock *)&ptr): %lu\n", ((HeapBlock *)&ptr));
 
   free_block->size += sizeof(ptr) + block_head->size;
   alloc_block->size -= sizeof(ptr) + block_head->size;
@@ -81,13 +59,27 @@ void orig_free(void *ptr) {
 
 int main() {
   initialize_block();
+
+  assert(alloc_block->size == 0);
+  assert(free_block->size == sizeof(arena));
+
   int *zero = (int *)orig_malloc(sizeof(int));
-  if (zero == NULL) exit(0);
   *zero = 0;
 
+  assert(alloc_block->size == (sizeof(HeapBlock *) + sizeof(*zero)));
+  assert(free_block->size == sizeof(arena) - (sizeof(HeapBlock *) + sizeof(*zero)));
+
+  assert(zero == &arena[0] + (sizeof(HeapBlock) / sizeof(int)));
+
   int *one = (int *)orig_malloc(sizeof(int));
-  if (one == NULL) exit(0);
   *one = 1;
+
+  assert(alloc_block->size == (sizeof(HeapBlock *) * 2 + sizeof(*zero) + sizeof(*one)));
+  assert(free_block->size == sizeof(arena) - (sizeof(HeapBlock *) * 2 + sizeof(*zero + sizeof(*one))));
+
+  struct HeapBlock* block = (struct HeapBlock*)zero - 1;
+  assert((struct HeapBlock*)one - 1 == block->next);
+  assert(one == zero + 1 + (sizeof(HeapBlock) / (sizeof(zero) + 1)));
 
   printf("--- result ---\n");
   printf("*zero %d\n", *zero);
@@ -107,11 +99,9 @@ int main() {
   printf("--- end ---\n");
 
   int *two = (int *)orig_malloc(sizeof(int));
-  if (two == NULL) exit(0);
   *two = 2;
 
   int *three = (int *)orig_malloc(sizeof(int));
-  if (three == NULL) exit(0);
   *three = 3;
 
   // TODO Check new alloc addres
