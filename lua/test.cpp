a2560dd952a95e738ff7a1eade01244cf81e5498
sea_mountain <seaforestmountain@gmail.com>
2014-06-22 16:03:33 +0900

    Create minimum sample

--- lua/test.cpp
+++ lua/test.cpp
@@ -14,25 +14,7 @@ void print_msg(lua_State* s, char* msg) {
 int main(int argc, char* argv[])
 {
   // create new Lua state
-  lua_State *lua_state;
-  lua_state = luaL_newstate();
-
-  // load Lua libraries
-  static const luaL_Reg lualibs[] =
-  {
-    { "base", luaopen_base },
-    { NULL, NULL}
-  };
-
-  const luaL_Reg *lib = lualibs;
-  for(; lib->func != NULL; lib++)
-  {
-    lib->func(lua_state);
-    lua_settop(lua_state, 0);
-  }
-
-  // run the Lua script
-  luaL_dofile(lua_state, "helloworld.lua");
+  lua_State *lua_state = luaL_newstate();
 
   char msg[] = "hoge";
   print_msg(lua_state, msg);
