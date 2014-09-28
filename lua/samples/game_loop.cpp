850cc1f6042b455b81415cfc19c9109d834d826b
sea_mountain <seaforestmountain@gmail.com>
2014-09-28 14:52:04 +0900

    Add lua call

--- lua/samples/game_loop.cpp
+++ lua/samples/game_loop.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include "lua-5.2.3/src/lua.hpp"
 
 int fps = 30;
 int frame_count = fps;
@@ -10,28 +11,39 @@ int TARGET_FPS = 60;
 int TARGET_FRAME_TIME = 1000 / TARGET_FPS;
 
 
-void Update() {
+void Update(lua_State *l) {
   if (frame_count == TARGET_FPS) {
     // http://www.c-lang.net/clock
     frame_count = 0;
   } else {
     frame_count++;
   }
+
+  if (luaL_dofile(l, "./game_loop/update.lua")) {
+    printf("error: %s\n", lua_tostring(l, -1));
+  }
 }
 
-void Draw() {
+void Draw(lua_State *l) {
   // call lua func
   printf("%d \n", frame_count);
+
+  if (luaL_dofile(l, "./game_loop/draw.lua")) {
+    printf("error: %s\n", lua_tostring(l, -1));
+  }
 }
 
 int main(int argc, char* argv[])
 {
+  lua_State *l = luaL_newstate();
+  luaL_openlibs(l);
+
   while (true) {
     clock_t begin = clock();
 
     // GameLoop
-    Update();
-    Draw();
+    Update(l);
+    Draw(l);
 
     // wait
     clock_t end = clock();
