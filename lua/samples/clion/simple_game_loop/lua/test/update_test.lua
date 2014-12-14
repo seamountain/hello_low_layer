267d203e4de7fc5941c12909f1dc59bf03b1103b
sea_mountain <seaforestmountain@gmail.com>
2014-12-14 20:22:38 +0900

    Update test

--- lua/samples/clion/simple_game_loop/lua/test/update_test.lua
+++ lua/samples/clion/simple_game_loop/lua/test/update_test.lua
@@ -1,20 +1,33 @@
 -- Run this at not test but lua directory
 require("update")
+require("data")
+require("data_manager")
 
 -- http://lua-users.org/wiki/UnitTesting
 require('test/luaunit/luaunit')
 
 TestUpdate = {}
 function TestUpdate:test_turn_opposit_direction()
-  left = 0
-  top = 1
-  right = 2
-  buttom = 3
+  local left = 0
+  local top = 1
+  local right = 2
+  local buttom = 3
 
-  assertEquals(turn_opposite_direction(left), right)
-  assertEquals(turn_opposite_direction(top), buttom)
-  assertEquals(turn_opposite_direction(right), left)
-  assertEquals(turn_opposite_direction(buttom), top)
+  d = PointData.new(0, 0, 0, 0, 0, left)
+  turn_opposite_direction(d)
+  assertEquals(d.d, right)
+
+  d.d = top
+  turn_opposite_direction(d)
+  assertEquals(d.d, buttom)
+
+  d.d = right
+  turn_opposite_direction(d)
+  assertEquals(d.d, left)
+
+  d.d = buttom
+  turn_opposite_direction(d)
+  assertEquals(d.d, top)
 end
 
 LuaUnit:run()
