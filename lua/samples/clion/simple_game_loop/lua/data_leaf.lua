6bdc34642b5adb92fd5e73da4a62d5af16dcee9b
sea_mountain <seaforestmountain@gmail.com>
2015-01-25 17:40:03 +0900

    Add space pertitioning

---
+++ lua/samples/clion/simple_game_loop/lua/data_leaf.lua
@@ -0,0 +1,10 @@
+DataLeaf = {}
+DataLeaf.new = function (d)
+  local obj = {}
+
+  obj.data = d
+  obj.p_node = {}
+  obj.index = 0
+
+  return obj
+end
--- lua/samples/clion/simple_game_loop/lua/data_manager.lua
+++ lua/samples/clion/simple_game_loop/lua/data_manager.lua
@@ -1,10 +1,20 @@
 data_list = {}
+
 function register_data(x, y, color_id)
   local w = 10
   local h = 10
   local dir = math.random(0, 3)
   local d = Data.new(x, y, color_id, w, h, dir)
   table.insert(data_list, d)
+
+  add_to_tree(d)
+end
+
+function register_data_with_params(x, y, color_id, w, h, dir)
+  local d = Data.new(x, y, color_id, w, h, dir)
+  table.insert(data_list, d)
+
+  add_to_tree(d)
 end
 
 function data_num()
@@ -13,9 +23,10 @@ end
 
 function hit_another_data(index)
   local d = data_list[index]
-  local list_size = #data_list
+  --local list_size = #data_list
+  local list_size = #d.data_leaf.p_node
   for i = 1, list_size do
-    local target = data_list[i]
+    local target = d.data_leaf.p_node[i].data
     if target ~= d then
       local x1 = d.x
       local w1 = d.w
@@ -28,7 +39,6 @@ function hit_another_data(index)
       local h2 = target.h
 
       if ((x1 - w2 <= x2 and x2 <= x1 + w1) and (y1 - h2 <= y2 and y2 <= y1 + h1)) then
-
         -- TODO Add splitting data
         turn_opposite_direction(d)
         turn_opposite_direction(target)
@@ -37,7 +47,7 @@ function hit_another_data(index)
   end
 end
 
--- TODO data_list size test afte removing
+-- TODO data_list size test after removing
 function move_data()
   local list_size = #data_list
   local i = 1
@@ -47,6 +57,7 @@ function move_data()
     -- increment just at not removed situation
     if 0 <= changed_num then
       hit_another_data(i)
+      update_node(i)
       i = i + 1
     end
     list_size = list_size + changed_num
@@ -62,12 +73,13 @@ function split_data(index)
   --TODO Remove small data
   local min_size = 2
   if d.w <= min_size or d.h <= min_size then
+    remove_leaf(d)
+    d.data_leaf = nil
     table.remove(data_list, index)
     return -1
   end
 
   local slide_rate = math.random(890, 950) / 1000
-  local splited_data = Data.new(d.x * slide_rate, d.y * slide_rate, d.color_id, d.w, d.h, d.d)
-  table.insert(data_list, splited_data)
+  register_data_with_params(d.x * slide_rate, d.y * slide_rate, d.color_id, d.w, d.h, d.d)
   return 1
 end
